43053a21d859245b8ec4d0dad65fdcb991f0a44c
hardik.devani <hardik.devani@coruscate.in>
2020-08-13 19:14:29 +0530

    msg

---
+++ src/components/blog.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+
+export default class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-blog" data-section="blog">
+			<div className="colorlib-narrow-content">
+				<div className="row">
+					<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+					<span className="heading-meta">Read</span>
+					<h2 className="colorlib-heading">Recent Blog</h2>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div className="blog-entry">
+						<a href="blog.html" className="blog-img"><img src="images/blog-1.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+						<div className="desc">
+							<span><small>April 14, 2018 </small> | <small> Web Design </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="blog.html">A list of 40+ investors investing</a></h3>
+							<p>Minimum $500 for a single e-scooter, $10000-$20000 for the e-scooter app, minimum $100000 of insurance</p>
+						</div>
+					</div>
+					</div>
+					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
+					<div className="blog-entry">
+						<a href="blog.html" className="blog-img"><img src="images/blog-2.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+						<div className="desc">
+							<span><small>April 14, 2018 </small> | <small> Web Design </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="blog.html">e-scooter sharing business, Canada</a></h3>
+							<p>With many Canadian cities legalizing the use of e-scooters on public roads, the Canadian micro-mobility market is going through</p>
+						</div>
+					</div>
+					</div>
+					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div className="blog-entry">
+						<a href="blog.html" className="blog-img"><img src="images/blog-3.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+						<div className="desc">
+							<span><small>April 14, 2018 </small> | <small> Inspiration </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="blog.html">e-scooter sharing business in Calgary, Canada in 3 easy steps</a></h3>
+							<p>For visionary entrepreneurs who hanker after e-scooter app development in Canada, We’ve recently put forth a detailed blog. </p>
+						</div>
+					</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div className="blog-entry">
+						<a href="blog.html" className="blog-img"><img src="images/blog-4.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+						<div className="desc">
+							<span><small>April 14, 2018 </small> | <small> Web Design </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="blog.html">Build an e-scooter app and start running on private property</a></h3>
+							<p>Micro-mobility startups which have raised over $500 million are inspiring many new entrepreneurs</p>
+						</div>
+					</div>
+					</div>
+					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
+					<div className="blog-entry">
+						<a href="blog.html" className="blog-img"><img src="images/blog-5.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+						<div className="desc">
+							<span><small>April 14, 2018 </small> | <small> Web Design </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="blog.html">Build an e-scooter app like Veo which illustrates a master plan to launch the e-scooter</a></h3>
+							<p>The global micro-mobility market has been going through rectification since late 2019.</p>
+						</div>
+					</div>
+					</div>
+					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div className="blog-entry">
+						<a href="blog.html" className="blog-img"><img src="images/blog-6.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+						<div className="desc">
+							<span><small>April 14, 2018 </small> | <small> Inspiration </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="blog.html">Belgium is emerging as Europe’s most profitable micro-mobility market.</a></h3>
+							<p>Unlike London, many cities of the European countries are witnessing a transportation revolution. </p>
+						</div>
+					</div>
+					</div>
+				</div>
+				{/* <div className="row">
+					<div className="col-md-12 animate-box">
+					<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+					</div>
+				</div> */}
+			</div>
+			</section>
+      </div>
+    )
+  }
+}
---
+++ src/components/projects.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+				<section className="colorlib-work" data-section="projects">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+								<span className="heading-meta">My Work Expertise</span>
+								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Coruscate Solution</a></h3>
+											<span>Wordpress</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="https://www.coruscatesolution.com/"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Melbourne</a></h3>
+											<span>Wordpress</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Tensitron</a></h3>
+											<span>Wordpress</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
+								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Bizmalawi</a></h3>
+											<span>Wordpress</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">eScooter</a></h3>
+											<span>Wordpress</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Divine Blog</a></h3>
+											<span>Wordpress</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						{/* <div className="row">
+							<div className="col-md-12 animate-box">
+								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+							</div>
+						</div> */}
+					</div>
+				</section>
+      </div>
+    )
+  }
+}
