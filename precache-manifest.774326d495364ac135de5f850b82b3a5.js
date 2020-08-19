cdbe3cb9c1eb85bc5d8c903280ee50b54374b482
hardik.devani <hardik.devani@coruscate.in>
2020-08-19 19:18:24 +0530

    Updates

---
+++ precache-manifest.774326d495364ac135de5f850b82b3a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "ebc0e43a8c8cc709a166",
+    "url": "/portfolio/static/js/main.ebc0e43a.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ebc0e43a8c8cc709a166",
+    "url": "/portfolio/static/css/main.53884008.chunk.css"
+  },
+  {
+    "revision": "7fa84a161947e79a477a583351afbcfe",
+    "url": "/portfolio/index.html"
+  }
+];
